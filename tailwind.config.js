cbf103c03d522d34b5f7efcd34b6108b0a420eb2
SamuelAMZ <>
2024-09-15 19:05:04 +0000

    setting up the project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+
+  daisyui: {
+    themes: ["light"],
+  },
+  plugins: [
+    require("daisyui"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+  ],
+  variants: {
+    scrollbar: ["rounded"],
+  },
+};
